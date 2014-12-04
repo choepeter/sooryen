b5d5637d792c253841ca4c681b029902ffd8a582
pchoe <pchoe@pchoe-PC>
2014-12-04 01:21:00 -0500

    initial commit

---
+++ sooryen/src/com/sooryen/swchoe/model/Item.java
@@ -0,0 +1,36 @@
+package com.sooryen.swchoe.model;
+
+public class Item {
+	private String id;
+	private String title;
+	private String price;
+	
+	public Item(String title, String price){
+		this.title = title;
+		this.price = price;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
