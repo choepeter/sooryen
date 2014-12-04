1fd19fc570d5e8efee510f11294e48211da525ef
pchoe <pchoe@pchoe-PC>
2014-12-04 01:37:39 -0500

    CraigsListExtractor redo

---
+++ sooryen/src/com/sooryen/swchoe/CraigsListIndexer.java
@@ -0,0 +1,29 @@
+package com.sooryen.swchoe;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class CraigsListIndexer {
+
+	public static Elements getElementsByClass(Document doc, String className){
+		Elements elements = doc.getElementsByClass(className);
+		return elements;
+	}
+	
+	public static Document getDocument(String url) throws IOException{
+		Document document = Jsoup.connect(url).get();
+		return document;
+	}
+	
+	public static String getText(Element element, String className){
+		String text = new String();
+		Elements elements = element.select("." + className);
+		if(elements.size() == 0) return "";
+		text = elements.get(0).text();
+		return text;
+	}
+}
